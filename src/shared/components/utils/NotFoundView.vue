c567a3dc7fab0a9050f64adb0ad2357604790aec
lucasmapaiva <lucaspaivadev@gmail.com>
2025-07-17 15:07:03 -0400

    criando arquivo de notfound

---
+++ src/shared/components/utils/NotFoundView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex flex-col items-center justify-center gap-4">
+    <h2 class="text-black text-3xl font-bold">404 - Página Não Encontrada</h2>
+    <p class="text-black text-lg">A página que você está procurando não existe.</p>
+    <router-link to="/" class="text-black underline hover:text-blue-600 transition"
+      >Voltar para a Home</router-link
+    >
+  </div>
+</template>
